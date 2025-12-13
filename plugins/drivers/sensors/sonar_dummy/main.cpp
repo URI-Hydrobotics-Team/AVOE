c6fc64cb0086fb28b53b3d0a43ad8862a236f4cb
corvuscorax12 <aldawson1@gmail.com>
2025-12-13 01:23:58 -0500

    a big train wreck feat. cmake

---
+++ plugins/drivers/sensors/sonar_dummy/main.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <iostream>
+#include "../../../../Sonar/ping-cppcstred/src/device/ping-device-ping1d.h"
+/**
+ * @brief Test ping 1D devices
+ */
+int main()
+{
+
+
+
+    Ping1d device = Ping1d();
+    //
+    device.request(5,30);
+    bool isgood = true;
+    while (isgood)
+    {
+        device.request(1211,90);
+        std::cout <<"\nVersion:" << (int)device.protocol_version.version_major<< "." << (int)device.protocol_version.version_minor << "."<< (int)device.protocol_version.version_patch;
+        std::cout << "\n" << "Distance:" <<   (int)device.distance_simple_data.distance;
+        std::cout << "\n" << "CONF:" <<   (int)device.distance_simple_data.confidence;
+        device.set_gain_setting(2);
+        /* code */
+    }
+    
+
+
+
+    return 0;
+}
