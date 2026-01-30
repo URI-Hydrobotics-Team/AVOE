57480b30591d5d29dfa5df476f3785325a1d9987
roach350 <dome.c4129@gmail.com>
2026-01-30 17:25:03 -0500

    typo fixed

--- core/motor.h
+++ core/motor.h
@@ -34,7 +34,7 @@ class motor_t{
 
 		motor_t(size_t fields);
 		~motor_t();
-		void init(const char *l, const char *n, const char *v, const char *p, const char *t, vector_t pos, vector_t dir);
+		void init(const char *l, const char *m, const char *v, const char *p, const char *t, vector_t pos, vector_t dir);
 		char *getModel();
 		char *getLabel();
 		char *getVendor();
