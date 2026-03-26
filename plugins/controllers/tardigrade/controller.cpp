f1cd2d3b2fc9830689d407b6a8b54583654bfb54
Faos6151 <Logan.Rekowski@uri.edu>
2026-03-26 19:30:16 -0400

    Lateral Movement iteration v1 implemented

--- plugins/controllers/tardigrade/controller.cpp
+++ plugins/controllers/tardigrade/controller.cpp
@@ -3,14 +3,16 @@
 
 
 
-tardigrade_controller_t::tardigrade_basic_controller_t(){
+tardigrade_controller_t::tardigrade_controller_t(){
 	name = nullptr;
 	vehicle = nullptr;
 	motorTable = nullptr;
 }
 
-
-
+tardigrade_controller_t::~tardigrade_controller_t(){
+    delete[] name;
+    delete[] vehicle;
+}
 
 void tardigrade_controller_t::init(const char *n, vehicle_t *vehicle_in){
     name = new char[NAME_LEN]; //default is 32
@@ -19,19 +21,15 @@ void tardigrade_controller_t::init(const char *n, vehicle_t *vehicle_in){
     motorTable = vehicle->getMotorTable();
 }
 
-tardigrade_controller_t::~tardigrade_basic_controller_t(){
-    delete[] name;
-    delete[] vehicle;
-}
-void tardigrade_basic_controller_t::send_vector(vector_t thrust_vector){ //we assume pivot and orgin are about the center of the robot
+void tardigrade_controller_t::send_translation_vector(vector_t thrust_vector){ //we assume pivot and orgin are about the center of the robot
     double x = thrust_vector.x;
     double y = thrust_vector.y;
     double z = thrust_vector.z;
 
-	/*
+	
     for(size_t i = 0; i < 6; ++i){
         //Sets motor values after calculating the x,y,z total
-        double total = (x * thrust_table[i][0]) + (y * thrust_table[i][1]) + (z * thrust_table[i][2]);
+        double total = (x * translation_thrust_table[i][0]) + (y * translation_thrust_table[i][1]) + (z * translation_thrust_table[i][2]);
         // Gets motor from vehicle objects motor table and writes the intended value to it 
         // (only works if motors in correct order of thrust_table)
         std::string str = std::to_string(total);
@@ -40,5 +38,23 @@ void tardigrade_basic_controller_t::send_vector(vector_t thrust_vector){ //we as
         std::strncpy(temp, std::to_string(total).c_str(), sizeof(temp));
         motorTable[i]->write(temp,0,sizeof(temp));
     }
-    */
-} 
+}
+void tardigrade_controller_t::send_lateral_vector(vector_t thrust_vector){
+    //Rotational movement vectors
+    double rx = thrust_vector.x;
+    double ry = thrust_vector.y;
+    double rz = thrust_vector.z;
+
+
+    for(size_t i = 0; i < 6; ++i){
+        //Sets motor values after calculating the rx,ry,rz total
+        double total = (rx * lateral_thrust_table[i][0]) + (ry * lateral_thrust_table[i][1]) + (rz * lateral_thrust_table[i][2]);
+        // Gets motor from vehicle objects motor table and writes the intended value to it 
+        // (only works if motors in correct order of thrust_table)
+        std::string str = std::to_string(total);
+        char temp[str.length()];
+
+        std::strncpy(temp, std::to_string(total).c_str(), sizeof(temp));
+        motorTable[i]->write(temp,0,sizeof(temp));
+    }
+}
--- plugins/controllers/tardigrade/controller.h
+++ plugins/controllers/tardigrade/controller.h
@@ -20,7 +20,7 @@ class tardigrade_controller_t{
 	private:
 		char *name; //name and version string (unimportant)
 		vehicle_t *vehicle; //Pull Motors from Vehicle object
-		float thrust_table[6][3] = {
+		float translation_thrust_table[6][3] = {
 			{0.0, 0.0, 1.0},	//SH
 			{0.0, 0.0, 0.5},	//BSH
 			{0.0, 0.0, 0.5}, 	//BPH
@@ -28,16 +28,26 @@ class tardigrade_controller_t{
 			{1, 0.4, 0.0}, //SS
 			{1, -0.4, 0.0}	//PS
 		};
+
+		float lateral_thrust_table[6][3] = {
+		//  roll,pitch,yaw
+			{0.0, 1.0, 0},	//SH
+			{0.0, 0.5, 0},	//BSH
+			{0.0, 0.5, 0}, 	//BPH
+			{0.0, 0, 0.6}, 	//Y
+			{0, 0, 0.4}, //SS
+			{0, 0, -0.4}	//PS
+		};
 		motor_t** motorTable;
 
 
 	public:
 		tardigrade_controller_t();
 		tardigrade_controller_t(const char *n, vehicle_t *vehicle_in);
-		~tardigrade__controller_t();
-		void send_vector(vector_t thrust_vector, vector_t thrust_point, vector_t pivot_point);
-
-
+		~tardigrade_controller_t();
+		void init(const char *n, vehicle_t *vehicle_in);
+		void send_translation_vector(vector_t thrust_vector);
+		void send_lateral_vector(vector_t thrust_vector);
 };
 
 #endif
