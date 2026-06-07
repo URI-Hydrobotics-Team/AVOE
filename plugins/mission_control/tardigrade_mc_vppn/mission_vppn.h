5fdd42d558eccbad31125c867737aff89fd020da
roach350 <dome.c4129@gmail.com>
2026-06-06 23:44:53 -0400

    misc.

--- plugins/mission_control/tardigrade_mc_vppn/mission_vppn.h
+++ plugins/mission_control/tardigrade_mc_vppn/mission_vppn.h
@@ -1,23 +1,19 @@
 #define KEYWORD_LEN 128
 
 
-class mission_vppn{
+class mission_vppn_t{
 
 	private:
 
 		char keyword[KEYWORD_LEN];
 		size_t keyword_len;
-		
+		uint8_t type; //main, hold, dr
+		float speed;	
 
 	public:
 
-		mission_vppn;
+		mission_vppn(const char *keyword_, size_t keyword_len_, uint8_t type_, float speed_);
 		~mission_vppn;
-		void init;
-
-
-
-
 
 
 };
