eab565f780e455cfdd5dc418bd707d8836cb504e
roach350 <dome.c4129@gmail.com>
2026-06-09 19:20:38 -0400

    small changes

--- plugins/mission_control/tardigrade_mc_vppn/mc_vppn.cpp
+++ plugins/mission_control/tardigrade_mc_vppn/mc_vppn.cpp
@@ -6,17 +6,15 @@ tardigrade_mc_vppn_t::tardigrade_mc_vppn_t();
 tardigrade_mc_vppn_t::~tardigrade_mc_vppn_t();
 
 
-void tardigrade_mc_vppn_t::init(vppn_mission_t *vppn_main_object, vppn_mission_t *vppn_dr_object, size_t vppn_main_object_len_, size_t vppn_dr_object_len_, targrade_controller_t *controller_, char *keyword_str_, size_t keyword_len){
+void tardigrade_mc_vppn_t::init(vppn_mission_t *vppn_main_object, vppn_mission_t *vppn_dr_object, size_t vppn_main_object_len_, size_t vppn_dr_object_len_, vector_t *offset_, char *keyword_str_){
 
 
 	vppn_main_object_index = 0;
 	vppn_dr_object_index = 0;
 	vppn_main_object_len = vppn_main_object_len_;
 	vppn_dr_object_len = vppn_dr_object_len_;
-	controller = controller_;
 	keyword_str = keyword_str_;
-	keyword_len = keyword_len_;
-
+	offset_vector = offset;
 
 }
 
--- plugins/mission_control/tardigrade_mc_vppn/mc_vppn.h
+++ plugins/mission_control/tardigrade_mc_vppn/mc_vppn.h
@@ -15,18 +15,17 @@ class tardigrade_mc_vppn_t{
 		vppn_mission_t *vppn_dr_object_ptr;
 		size_t vppn_main_object_len;
 		size_t vppn_dr_object_len;
-
+		vector_t *offset_vector;
 		size_t vppn_main_object_index;
 		size_t vppn_dr_object_index;
 		targrade_controller_t *controller;
 		char *keyword_str;
-		size_t keyword_len
 
 	public:
 		
 		tardigrade_mc_vppn_t;
 		~tardigrade_mc_vppn_t;
-		void init(vppn_mission_t *vppn_main_object, vppn_mission_t *vppn_dr_object, size_t vppn_main_object_len_, size_t vppn_dr_object_len_, targrade_controller_t *controller_, char *keyword_str_, size_t keyword_len_);
+		void init(vppn_mission_t *vppn_main_object, vppn_mission_t *vppn_dr_object, size_t vppn_main_object_len_, size_t vppn_dr_object_len_, vector_t *offset, char *keyword_str_);
 		void reset();
 		void refresh();
 		
