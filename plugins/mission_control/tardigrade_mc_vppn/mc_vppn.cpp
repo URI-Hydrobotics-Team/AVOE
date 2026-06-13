152f33b6d2e8577cd498a1634df13ac7b939862d
roach350 <dome.c4129@gmail.com>
2026-06-13 12:58:16 -0400

    a commit

--- plugins/mission_control/tardigrade_mc_vppn/mc_vppn.cpp
+++ plugins/mission_control/tardigrade_mc_vppn/mc_vppn.cpp
@@ -6,7 +6,7 @@ tardigrade_mc_vppn_t::tardigrade_mc_vppn_t();
 tardigrade_mc_vppn_t::~tardigrade_mc_vppn_t();
 
 
-void tardigrade_mc_vppn_t::init(vppn_mission_t *vppn_main_object, vppn_mission_t *vppn_dr_object, size_t vppn_main_object_len_, size_t vppn_dr_object_len_, vector_t *offset_, char *keyword_str_){
+void tardigrade_mc_vppn_t::init(vppn_mission_t *vppn_main_object, vppn_mission_t *vppn_dr_object, size_t vppn_main_object_len_, size_t vppn_dr_object_len_, vector_t *offset_, char *keyword_str_, vector_t *output_){
 
 
 	vppn_main_object_index = 0;
@@ -14,11 +14,13 @@ void tardigrade_mc_vppn_t::init(vppn_mission_t *vppn_main_object, vppn_mission_t
 	vppn_main_object_len = vppn_main_object_len_;
 	vppn_dr_object_len = vppn_dr_object_len_;
 	keyword_str = keyword_str_;
-	offset_vector = offset;
+	offset_vector = offset_;
+	output_vector = output_;
+	clock.reset();
 
 }
 
-tardrigrade_mc_vppn_::reset(){
+tardigrade_mc_vppn_::reset(){
 	vppn_main_object_index = 0;
 	vppn_dr_object_index = 0;
 	
@@ -31,13 +33,48 @@ tardrigrade_mc_vppn_::reset(){
 
 tardigrade_mc_vppn::refresh(){
 
+	vector_t output_temp(0, 0, 0);
+	
 	/*
-		read form socket
+		read from keyword
 		determine if keyword matches
-		send commands to comntroller to make adjustments
+			adjust output vector as necessary
+
+		handle no match case
 
 	*/
 
+	//offset_vector.x and offset_vector.y define the pixel offsets RXed for CV
+
+	// get our stuff
+
+	
+	bool dead_reckon = !(strncmp(keyword_str, mission->keyword, mission->keyword_len) == 0); //false if match, true if no
+	if (!dead_reckon){
+		clock.reset(); // update clock everytime we check and get a match
+	}	
+
+
+	int cv_x_offset = offset->x;
+	int cv_y_offset = offset->y;
+	float speed = offset->speed;
+
+
+	if (cv_x_offset > TARDIGRADE_MC_VPPN_PIXEL_BUFFER && !dead_reckon){
+
+		//make adjust
+
+	}
+
+
+	if (dead_reckon && clock.getElaspedTimeMS() > TARDIGRADE_MC_VPPN_DEAD_RECKON_DELAY){
+
+		//dead reckon now
+
+	}
+	
+	
+	
 
 
 	
--- plugins/mission_control/tardigrade_mc_vppn/mc_vppn.h
+++ plugins/mission_control/tardigrade_mc_vppn/mc_vppn.h
@@ -6,6 +6,10 @@
 
 */
 
+#define TARDIGRADE_MC_VPPN_YAW_RATE 0.1
+#define TARDIGRADE_MC_VPPN_PIXEL_BUFFER 10
+#define TARDIGRADE_MC_VPPN_DEAD_RECKON_DELAY 2000 //ms
+
 class tardigrade_mc_vppn_t{
 
 
@@ -15,17 +19,19 @@ class tardigrade_mc_vppn_t{
 		vppn_mission_t *vppn_dr_object_ptr;
 		size_t vppn_main_object_len;
 		size_t vppn_dr_object_len;
-		vector_t *offset_vector;
+		vector_t *offset_vector, *output_vector;
 		size_t vppn_main_object_index;
 		size_t vppn_dr_object_index;
 		targrade_controller_t *controller;
 		char *keyword_str;
 
+		avoe_time_t 
+
 	public:
 		
 		tardigrade_mc_vppn_t;
 		~tardigrade_mc_vppn_t;
-		void init(vppn_mission_t *vppn_main_object, vppn_mission_t *vppn_dr_object, size_t vppn_main_object_len_, size_t vppn_dr_object_len_, vector_t *offset, char *keyword_str_);
+		void init(vppn_mission_t *vppn_main_object, vppn_mission_t *vppn_dr_object, size_t vppn_main_object_len_, size_t vppn_dr_object_len_, vector_t *offset_, char *keyword_str_, vector_t *output_);
 		void reset();
 		void refresh();
 		
