d4e57242a00795a0ae99d970f992a9f07d0f5c98
Faos6151 <Logan.Rekowski@uri.edu>
2026-07-12 01:43:18 -0400

    Revert "Some MC changes and a outline for the MC testing method"
    This reverts commit 0d237445ceab9239142aa83dbf71de6c9aa507df.

--- plugins/mission_control/tardigrade_mc_relative/mc.cpp
+++ plugins/mission_control/tardigrade_mc_relative/mc.cpp
@@ -2,12 +2,8 @@
 
 
 
-tardigrade_mc_basic_t::tardigrade_mc_basic_t(){
-
-}
-tardigrade_mc_basic_t::~tardigrade_mc_basic_t(){
-    
-}
+tardigrade_mc_basic_t::tardigrade_mc_basic_t();
+~tardigrade_mc_basic_t::tardigrade_mc_basic_t();
 
 
 void tardigrade_mc_basic_t::init(tardigrade_controller_t *controller, tardigrade_mission_basic_t **missions, size_t mission_count){
--- plugins/mission_control/tardigrade_mc_relative/mc.h
+++ plugins/mission_control/tardigrade_mc_relative/mc.h
@@ -8,7 +8,7 @@
 #define TARDIGRADE_MC_BASIC_H
 
 #include "mission.h"
-#include "../controllers/tardigrade/controller.h"
+#include "../controlers/tardigrade/controller.h"
 #include "../middleware/general-sensor/imu.h"
 
 
@@ -18,14 +18,14 @@ class tardigrade_mc_basic_t{
 	private:
 		size_t mission_index;
 		size_t mission_size;
-		tardigrade_controller_t *controller;
+		tarsigrade_controller_t *controller;
 		tardigrade_mission_basic_t **mission_ptr;
 
 
 	public:
 
-		tardigrade_mc_basic_t();
-		~tardigrade_mc_basic_t();
+		mission_control_tardigrade_t();
+		~mission_control_tardigrade_t();
 		void init(tardigrade_controller_t *controller, tardigrade_mission_basic_t **missions, size_t mission_count);
 		void refresh(); // update
 		void start();
